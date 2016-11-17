73524f2f80a776bbb1274c68bc6d0e0dfa9fa6a1
Milt Reder <milt@anestuary.com>
2016-11-17 08:47:13 -0500

    datomic 0.9.5407, release 0.1.2

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.yetanalytics/testomic "0.1.1"
+(defproject com.yetanalytics/testomic "0.1.2"
   :description "Testing utilities and macros for use with datomic."
   :url "http://github.com/yetanalytics/testomic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.datomic/datomic-free "0.9.5385"
+                 [com.datomic/datomic-free "0.9.5407"
                   :exclusions [commons-codec]]
                  [io.rkn/conformity "0.4.0"]
                  [crypto-random "1.2.0"]])
