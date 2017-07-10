999d1f22cf7a7e61a8af0145bf8f087d343546c9
Milt Reder <milt@yetanalytics.com>
2017-07-10 13:59:01 -0400

    0.1.4-alpha1 with "with" macros

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject com.yetanalytics/testomic "0.1.4-SNAPSHOT"
+(defproject com.yetanalytics/testomic "0.1.4-alpha1"
   :description "Testing utilities and macros for use with datomic."
   :url "http://github.com/yetanalytics/testomic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.datomic/datomic-free "0.9.5554"
-                  :exclusions [commons-codec]]
-                 [io.rkn/conformity "0.4.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [com.datomic/datomic-free "0.9.5561.50"]
+                 [io.rkn/conformity "0.5.0"]
                  [crypto-random "1.2.0"]])
